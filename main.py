0782842203ab0a9be1b3c46c3953be49d1896476
Rex Nowacki <nowacki.chris@gmail.com>
2023-02-27 22:27:24 -0700

    initial commit on dev

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-# cthulhu notes v0.01 :: mon jan 16 2023
-
+# cthulhu notes v0.01 :: mon feb 27 2023
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 import sys
 
 # Get the home directory
@@ -10,6 +9,35 @@ home_dir = os.path.expanduser("~")
 # Get the arguments from the command line
 args = sys.argv[1:]
 file_name = None
+
+# display today's notes
+if "--today" in args:
+    today = datetime.today().strftime('%Y-%m-%d')
+    notes_folder = os.path.join(home_dir, "notes", today)
+    if os.path.exists(notes_folder):
+        for file_name in os.listdir(notes_folder):
+            if file_name.endswith('.md'):
+                with open(os.path.join(notes_folder, file_name)) as file:
+                    print(file.read())
+                    exit()
+    else:
+        print("No notes found for today.")
+        exit()
+
+# yesterday's notes
+if "--yesterday" in args:
+    yesterday = (datetime.today() - timedelta(1)).strftime('%Y-%m-%d')
+    notes_folder = os.path.join(home_dir, "notes", yesterday)
+    if os.path.exists(notes_folder):
+        for file_name in os.listdir(notes_folder):
+            if file_name.endswith('.md'):
+                with open(os.path.join(notes_folder, file_name)) as file:
+                    print(file.read())
+                    exit()
+    else:
+        print("No notes found for yesterday.")
+        exit()
+
 
 if "-n" in args:
     # Get the index of the -n argument
@@ -24,8 +52,8 @@ note = " ".join(args)
 
 # Check for the "notes" directory
 notes_dir = os.path.join(home_dir, "notes")
-#if not os.path.exists(notes_dir):
-#    os.mkdir(notes_dir)
+if not os.path.exists(notes_dir):
+    os.mkdir(notes_dir)
 
 # Enter the "notes" directory
 os.chdir(notes_dir)
